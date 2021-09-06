bcc6cb25d6159e702bdf034cb7fbea31f85c4209
Alexey Shaytan <molsim@users.noreply.github.com>
2021-09-06 22:20:08 +0300

    Update reinit_histdb_local.sh

--- reinit_histdb_local.sh
+++ reinit_histdb_local.sh
@@ -25,8 +25,11 @@ python manage.py buildvarianttypes
 python manage.py buildvariantinfo -f
 
 python manage.py buildhmms
+
 # python manage.py extractvariants -f #This will download new nr if not present in dir
-python manage.py extractvariants -f --db nr_small_per10_v4
+#Line below is for express code testing
+python manage.py extractvariants -f --db https://www.dropbox.com/s/6lh26otjq0w8337/nr_test_small_352mb.gz
+
 python manage.py buildblastdb
 python manage.py buildblastdbs
 python manage.py classifyvariants -f
